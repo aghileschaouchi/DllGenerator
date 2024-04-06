dce38f2ee8abaeb9d29fdd3e53c83c31681dbb3a
aghileschaouchi <aghiles.chaouchi@outlook.com>
2024-04-07 01:46:45 +0200

    working noClip dll, injected successfully with extern dll injector

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,73 +1,84 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
-#include "pch.h"
+
 #include "../GameHackingEngine/include/core/WinCodeCave.h"
 #include "../GameHackingEngine/include/helper/Helper.h"
 
-#include <string>
+namespace
+{
+	const std::string messageBoxMessage("Injection location address");
+	const std::string messageBoxMessageDetach("Detaching in");
+	const std::string messageBoxMessageAttach("Attaching in");
+	const enum HookTypes { DESACTIVATE, ACTIVATE };
+	
+	const std::string baseModuleName("Tomb2.exe");
+	const std::string signature = "\x83\xC4\x04\xA8\x04\x75\x24";
+	const std::string mask = "xxxxxxx";
+	const DWORD _jumpAddress = 0x004276C9;
+}
 
 DWORD collisionJmpBack = 0;
-DWORD _instrucitonAddress = 0x004276C9;
+DWORD collisionInsAddress = 0;
 
 extern "C"
 {
-    __declspec(naked) void activateNoClip()
-    {
-        //the jump should ignore the 3 other instructions but we kept them to better understanding codecaving
-        //since our sig scan has "4" as a mask, starting from the second instruction (+2) and countring 4 bytes, all those inscrutions will be erased
-        //check back the video to better understand
-        __asm JMP [_instrucitonAddress]
-        __asm PUSH EDI
-        __asm PUSH ESI
-        __asm JMP[collisionJmpBack]
-    }
-    
-    __declspec(naked) void desactivateNoClip()
-    {
-        //replace by JNE
-        __asm JMP [_instrucitonAddress]
-        __asm PUSH EDI
-        __asm PUSH ESI
-        __asm JMP[collisionJmpBack]
-    }
+	#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+
+	__declspec(naked) void activateNoClip()
+	{
+		//the jump should ignore the 3 other instructions but we kept them to better understanding codecaving
+		//since our sig scan has "4" as a mask, starting from the second instruction (+2) and countring 4 bytes, all those inscrutions will be erased
+		//check back the video to better understand
+		__asm ADD ESP, 4
+		__asm TEST AL, 04
+		__asm JMP[_jumpAddress]
+	}
+
+	__declspec(naked) void desactivateNoClip()
+	{
+		__asm ADD ESP, 4
+		__asm TEST AL, 04
+		__asm JNE SHORT _jumpAddress
+	}
 }
 
-const enum HookTypes { DESACTIVATE, ACTIVATE };
 template<int TYPE = ACTIVATE>
 void initiateHooks()
 {
-    DWORD collisionInsAddress = ghe::codeCave::findPattern(baseModuleName, "\xA8\x04\x75\x24", "xxxx") + 2;
-    collisionJmpBack = collisionInsAddress + 0x4;
+	collisionInsAddress = ghe::codeCave::findPattern(baseModuleName, signature, mask);
+	collisionJmpBack = collisionInsAddress + static_cast<DWORD>(mask.size());
 
-    static const std::string messageBoxMessage("Injection location address : ");
-    helper::debugMessageBox<decltype(collisionInsAddress)>(messageBoxMessage, collisionInsAddress);
+	 //for debug
+	//helper::debugMessageBox<decltype(collisionInsAddress)>(messageBoxMessage, collisionInsAddress);
 
-    if constexpr (ACTIVATE)
-    {
-        ghe::codeCave::jumpCodeCave(reinterpret_cast<BYTE*>(collisionInsAddress), reinterpret_cast<DWORD>(activateNoClip), 4);
-    }
+	if constexpr (TYPE == ACTIVATE)
+	{
+		ghe::codeCave::jumpCodeCave(reinterpret_cast<BYTE*>(collisionInsAddress), reinterpret_cast<DWORD>(activateNoClip), static_cast<DWORD>(mask.size()));
+	}
 
-    else if constexpr (DESACTIVATE)
-    {
-        ghe::codeCave::jumpCodeCave(reinterpret_cast<BYTE*>(collisionInsAddress), reinterpret_cast<DWORD>(desactivateNoClip), 4);
-    }
+	else if constexpr (TYPE == DESACTIVATE)
+	{
+		ghe::codeCave::jumpCodeCave(reinterpret_cast<BYTE*>(collisionInsAddress), reinterpret_cast<DWORD>(desactivateNoClip), static_cast<DWORD>(mask.size()));
+	}
 }
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                     )
+BOOL APIENTRY DllMain(HMODULE hModule,
+	DWORD  ul_reason_for_call,
+	LPVOID lpReserved
+)
 {
-    switch (ul_reason_for_call)
-    {
-    case DLL_PROCESS_ATTACH:
-        initiateHooks<ACTIVATE>();
-        break;
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-    case DLL_PROCESS_DETACH:
-        initiateHooks<DESACTIVATE>();
-        break;
-    }
-    return TRUE;
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+		//helper::debugMessageBox<decltype(collisionInsAddress)>(messageBoxMessageAttach, collisionInsAddress); 
+		initiateHooks<ACTIVATE>();
+		break;
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		//helper::debugMessageBox<decltype(collisionInsAddress)>(messageBoxMessageDetach, collisionInsAddress);
+		initiateHooks<DESACTIVATE>();
+		break;
+	}
+	return TRUE;
 }
 
